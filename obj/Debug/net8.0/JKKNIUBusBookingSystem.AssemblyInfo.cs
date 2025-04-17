7a776bf483ebbd7fe9a1b3e56e1486eff73a4d90
irteja500@gmail.com <irteja500@gmail.com>
2025-04-17 19:34:36 +0600

    Adding screenshot for readmefile

--- obj/Debug/net8.0/JKKNIUBusBookingSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/JKKNIUBusBookingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JKKNIUBusBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37ea097e3b5bdb4e366a6ff31f8918c00e1cc789")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8617ce9bf94d0f74efdce73e1d4fa696b93018cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("JKKNIUBusBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JKKNIUBusBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
